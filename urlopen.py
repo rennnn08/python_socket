dae880a5c9958b1d331621464e6c6cf967560e59
Ren Yamamoto <rennnn0806@gmail.com>
2021-10-24 19:51:29 +0900

    First pull

---
+++ urlopen.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+def main():
+    mes='GET / HTTP1.1\r\n'
+    mes+='Host: "yahoo.co.jp\r\n'
+    mes+='Connection: close\r\n'
+    mes+='\r\n'
+
+    url = sys.argv
+
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((url[1],80))
+
+    s.send(mes.encode("utf-8"))
+
+    data_sum = ''
+
+    while True:
+        data = s.recv(1024).decode("utf-8") 
+        data_sum = data_sum + data 
+        if not data:
+            break
+
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+
+    with open ('index.html', 'w', encoding='utf-8') as o:
+        o.write(data_sum)
+
+if __name__ == '__main__':
+    main()
